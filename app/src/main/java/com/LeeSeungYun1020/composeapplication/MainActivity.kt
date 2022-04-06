1958c1743c7864f44b1da6ec465ee4f166f07d97
Lee Seung Yun <ileilliat@gmail.com>
2022-04-06 17:41:46 +0900

    List and animations

--- app/src/main/java/com/LeeSeungYun1020/composeapplication/MainActivity.kt
+++ app/src/main/java/com/LeeSeungYun1020/composeapplication/MainActivity.kt
@@ -4,16 +4,22 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,7 +30,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeApplicationTheme {
-                MessageCard(Message("Android", "Jetpack Compose"))
+                Conversation(messages = SampleData.conversationSample)
             }
         }
     }
@@ -34,19 +40,28 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Row(modifier = Modifier.padding(all = 8.dp)) {
+    Row(modifier = Modifier.padding(8.dp)) {
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "Profile",
+            contentDescription = "profile",
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondaryVariant, CircleShape)
         )
 
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
+        val surfaceColor: Color by animateColorAsState(
+            targetValue =
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+        Column(modifier = Modifier.clickable {
+            isExpanded = !isExpanded
+        }) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
@@ -55,16 +70,21 @@ fun MessageCard(msg: Message) {
 
             Spacer(modifier = Modifier.height(4.dp))
 
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.body2
                 )
             }
         }
     }
-
 }
 
 @Preview(
@@ -79,5 +99,22 @@ fun MessageCard(msg: Message) {
 fun PreviewMessageCard() {
     ComposeApplicationTheme {
         MessageCard(msg = Message("Colleague", "Hey, take a look at this"))
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(msg = message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    ComposeApplicationTheme {
+        Conversation(messages = SampleData.conversationSample)
     }
 }
