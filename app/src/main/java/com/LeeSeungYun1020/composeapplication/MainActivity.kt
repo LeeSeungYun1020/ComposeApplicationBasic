eaa2f336c54e7efb19769470e952f5061f28036b
Lee Seung Yun <ileilliat@gmail.com>
2022-04-14 00:03:53 +0900

    Layouts in Jetpack Compose - Modifier

--- app/src/main/java/com/LeeSeungYun1020/composeapplication/MainActivity.kt
+++ app/src/main/java/com/LeeSeungYun1020/composeapplication/MainActivity.kt
@@ -4,22 +4,20 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ExpandLess
-import androidx.compose.material.icons.filled.ExpandMore
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,118 +36,43 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
-    if (shouldShowOnboarding) {
-        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
-    } else {
-        Greetings()
-    }
+    PhotographerCard()
 }
 
 @Composable
-fun Greetings(names: List<String> = List(1000) { "from $it" }) {
-    Surface(color = MaterialTheme.colors.background) {
-        Column(modifier = Modifier.padding(vertical = 4.dp)) {
-            LazyColumn {
-                item {
-                    Text(
-                        text = "This is Greetings",
-                        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
-                        style = MaterialTheme.typography.h4
-                    )
-                }
-                items(items = names) { name ->
-                    Greeting(name = name)
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun CardContents(name: String) {
-    var expanded by remember {
-        mutableStateOf(false)
-    }
-    Row(
-        modifier = Modifier
-            .padding(24.dp)
-            .animateContentSize(
-                animationSpec = spring(
-                    dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessLow
-                )
-            )
-    ) {
-        Column(
-            modifier = Modifier.weight(1f)
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(modifier
+        .padding(8.dp)
+        .clip(RoundedCornerShape(4.dp))
+        .background(MaterialTheme.colors.surface)
+        .clickable { }
+        .padding(16.dp)) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
         ) {
-            Text(text = "Hello,")
-            Text(
-                text = name, style = MaterialTheme.typography.h4.copy(
-                    fontWeight = FontWeight.ExtraBold, fontFamily = FontFamily.Serif
-                )
-            )
-            if (expanded) {
-                Text(
-                    text = ("Composem ipsum color sit lazy, " + "padding theme elit, sed do bouncy. ").repeat(
-                        4
-                    )
-                )
-            }
-        }
-        IconButton(onClick = { expanded = !expanded }) {
-            Icon(
-                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
-                contentDescription = if (expanded) stringResource(R.string.expand_less) else stringResource(
-                    R.string.expand_more
-                )
-            )
-        }
-    }
-}
 
-@Composable
-private fun Greeting(name: String) {
-    Card(
-        backgroundColor = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
-    ) {
-        CardContents(name)
-    }
-}
-
-@Composable
-fun OnboardingScreen(onContinueClicked: () -> Unit) {
-    Surface {
+        }
         Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
         ) {
-            Text("Welcome to the Basics Codelab!")
-            Button(
-                modifier = Modifier.padding(vertical = 24.dp), onClick = onContinueClicked
-            ) {
-                Text("Continue")
+            Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
             }
         }
     }
+
 }
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 320, uiMode = UI_MODE_NIGHT_YES)
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
-@Composable
-fun OnboardingPreview() {
-    ComposeApplicationTheme {
-        MyApp()
-    }
-}
-
-@Preview(showBackground = true, widthDp = 320, uiMode = UI_MODE_NIGHT_YES)
-@Preview(showBackground = true, widthDp = 320)
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeApplicationTheme {
-        Greetings()
+        PhotographerCard()
     }
 }
