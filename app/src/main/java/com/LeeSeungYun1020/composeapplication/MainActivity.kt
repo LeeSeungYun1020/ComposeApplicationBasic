2ffc75f0f11c6be9414fe2be9e9e45db1bcb88f6
Lee Seung Yun <ileilliat@gmail.com>
2022-04-14 15:08:49 +0900

    Layouts in Jetpack Compose - Constraint Layout

--- app/src/main/java/com/LeeSeungYun1020/composeapplication/MainActivity.kt
+++ app/src/main/java/com/LeeSeungYun1020/composeapplication/MainActivity.kt
@@ -4,23 +4,13 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.LeeSeungYun1020.composeapplication.ui.theme.ComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,124 +24,19 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-val topics = listOf(
-    "Arts & Crafts",
-    "Beauty",
-    "Books",
-    "Business",
-    "Comics",
-    "Culinary",
-    "Design",
-    "Fashion",
-    "Film",
-    "History",
-    "Maths",
-    "Music",
-    "People",
-    "Philosophy",
-    "Religion",
-    "Social sciences",
-    "Technology",
-    "TV",
-    "Writing"
-)
-
 @Composable
-fun LayoutsCodelab() {
-    Scaffold(topBar = {
-        TopAppBar(title = {
-            Text("LayoutsCodelab")
-        }, actions = {
-            IconButton(onClick = {}) {
-                Icon(Icons.Filled.ThumbUp, contentDescription = "Best")
-            }
+fun ConstraintLayoutContent() {
+    ConstraintLayout {
+        val (button, text) = createRefs()
+        Button(onClick = { /*TODO*/ }, modifier = Modifier.constrainAs(button) {
+            top.linkTo(parent.top, margin = 16.dp)
+        }) {
+            Text(text = "Button")
+        }
+        Text(text = "Text", Modifier.constrainAs(text) {
+            top.linkTo(button.bottom, margin = 16.dp)
+            centerHorizontallyTo(parent)
         })
-    }) { innerPadding ->
-        BodyContent(modifier = Modifier.padding(innerPadding), topics = topics)
-    }
-}
-
-@Composable
-fun StaggeredGrid(
-    modifier: Modifier = Modifier, rows: Int = 3, content: @Composable () -> Unit
-) {
-    Layout(
-        modifier = modifier, content = content
-    ) { measurables, constraints ->
-        val rowWidths = IntArray(rows) { 0 }
-        val rowHeights = IntArray(rows) { 0 }
-        val placeables = measurables.mapIndexed { index, measurable ->
-            val placeable = measurable.measure(constraints)
-
-            val row = index % rows
-            rowWidths[row] += placeable.width
-            rowHeights[row] = Math.max(rowHeights[row], placeable.height)
-
-            placeable
-        }
-
-        val width =
-            rowWidths.maxOrNull()?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth))
-                ?: constraints.minWidth
-
-        val height =
-            rowHeights.sumOf { it }.coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
-
-        val rowY = IntArray(rows) { 0 }
-        for (i in 1 until rows) {
-            rowY[i] = rowY[i - 1] + rowHeights[i - 1]
-        }
-
-        layout(width, height) {
-            val rowX = IntArray(rows) { 0 }
-
-            placeables.forEachIndexed { index, placeable ->
-                val row = index % rows
-                placeable.placeRelative(
-                    x = rowX[row], y = rowY[row]
-                )
-                rowX[row] += placeable.width
-            }
-        }
-    }
-}
-
-@Composable
-fun Chip(modifier: Modifier = Modifier, text: String) {
-    Card(
-        modifier = modifier,
-        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
-        shape = RoundedCornerShape(8.dp)
-    ) {
-        Row(
-            modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Box(
-                modifier = Modifier
-                    .size(16.dp, 16.dp)
-                    .background(color = MaterialTheme.colors.secondary)
-            )
-            Spacer(modifier = Modifier.width(4.dp))
-            Text(text = text)
-        }
-    }
-}
-
-@Composable
-fun BodyContent(modifier: Modifier = Modifier, topics: List<String>) {
-    Row(
-        Modifier
-            .background(Color.LightGray)
-            .padding(16.dp)
-            .size(200.dp)
-            .horizontalScroll(rememberScrollState())
-    ) {
-        StaggeredGrid(modifier = modifier) {
-            for (topic in topics) {
-                Chip(modifier = Modifier.padding(8.dp), text = topic)
-            }
-        }
     }
 }
 
@@ -160,6 +45,6 @@ fun BodyContent(modifier: Modifier = Modifier, topics: List<String>) {
 @Composable
 fun DefaultPreview() {
     ComposeApplicationTheme {
-        LayoutsCodelab()
+        ConstraintLayoutContent()
     }
 }
