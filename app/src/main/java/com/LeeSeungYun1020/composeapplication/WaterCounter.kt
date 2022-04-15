665056b02b17e6cd5892ce5c1716a87956e6dc63
Lee Seung Yun <ileilliat@gmail.com>
2022-04-15 23:13:57 +0900

    State in Jetpack Compose - Remember in Composition

--- app/src/main/java/com/LeeSeungYun1020/composeapplication/WaterCounter.kt
+++ app/src/main/java/com/LeeSeungYun1020/composeapplication/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.LeeSeungYun1020.composeapplication
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -12,14 +13,31 @@ import androidx.compose.ui.unit.dp
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-        if (count > 0) Text(text = "You've had $count glasses.")
-        Button(
-            onClick = { count++ },
-            modifier = Modifier.padding(top = 8.dp),
-            enabled = count < 10
-        ) {
-            Text(text = "Add one")
+        if (count > 0) {
+            var showTask by remember {
+                mutableStateOf(true)
+            }
+            if (showTask) {
+                WellnessTaskItem(taskName = "Have you taken your 15 minute walk today?",
+                    onClose = { showTask = false })
+            }
+            Text(text = "You've had $count glasses.")
         }
+        Row(modifier = Modifier.padding(top = 8.dp)) {
+            Button(
+                onClick = { count++ }, enabled = count < 10
+            ) {
+                Text(text = "Add one")
+            }
+            Button(
+                onClick = { count = 0 },
+                enabled = count > 0,
+                modifier = Modifier.padding(start = 8.dp)
+            ) {
+                Text(text = "Clear water count")
+            }
+        }
+
     }
 
 }
