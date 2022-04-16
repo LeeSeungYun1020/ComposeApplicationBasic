fab3c726d746d1420cade99c7271704bce69bb03
Lee Seung Yun <ileilliat@gmail.com>
2022-04-16 22:37:45 +0900

    Jetpack Compose Navigation - Setup

--- app/src/main/java/com/LeeSeungYun1020/composeapplication/ui/theme/Color.kt
+++ app/src/main/java/com/LeeSeungYun1020/composeapplication/ui/theme/Color.kt
@@ -14,14 +14,19 @@
  * limitations under the License.
  */
 
-package com.LeeSeungYun1020.composeapplication.ui
+package com.LeeSeungYun1020.composeapplication.ui.theme
 
+import androidx.compose.material.darkColors
 import androidx.compose.ui.graphics.Color
 
-val Purple100 = Color(0xFFE1BEE7)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-val Green300 = Color(0xFF81C784)
-val Green800 = Color(0xFF2E7D32)
-val Amber600 = Color(0xFFFFB300)
+val Green500 = Color(0xFF1EB980)
+val DarkBlue900 = Color(0xFF26282F)
+
+// Rally is always dark themed.
+val ColorPalette = darkColors(
+    primary = Green500,
+    surface = DarkBlue900,
+    onSurface = Color.White,
+    background = DarkBlue900,
+    onBackground = Color.White
+)
