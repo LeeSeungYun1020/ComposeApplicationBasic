22c42bae301d5c4b6e74958ab3f26861673c7b64
Lee Seung Yun <ileilliat@gmail.com>
2022-04-17 13:42:21 +0900

    Testing in Jetpack Compose - Debugging tests

--- app/src/androidTest/java/com/LeeSeungYun1020/composeapplication/TopAppBarTest.kt
+++ app/src/androidTest/java/com/LeeSeungYun1020/composeapplication/TopAppBarTest.kt
@@ -3,7 +3,10 @@ package com.LeeSeungYun1020.composeapplication
 import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
 import com.LeeSeungYun1020.composeapplication.ui.components.RallyTopAppBar
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -12,29 +15,29 @@ class TopAppBarTest {
     @get: Rule
     val composeTestRule = createComposeRule()
 
-    @Test
-    fun rallyTopAppBarTest() {
+    @Before
+    fun before() {
         val allScreens = RallyScreen.values().toList()
         composeTestRule.setContent {
             RallyTopAppBar(
-                allScreens = allScreens,
-                onTabSelected = {},
-                currentScreen = RallyScreen.Accounts
+                allScreens = allScreens, onTabSelected = {}, currentScreen = RallyScreen.Accounts
             )
         }
+    }
+
+    @Test
+    fun rallyTopAppBarTest() {
         Thread.sleep(500)
     }
 
     @Test
     fun rallyTopAppBarTest_currentTabSelected() {
-        val allScreens = RallyScreen.values().toList()
-        composeTestRule.setContent {
-            RallyTopAppBar(
-                allScreens = allScreens,
-                onTabSelected = {},
-                currentScreen = RallyScreen.Accounts
-            )
-        }
         composeTestRule.onNodeWithContentDescription(RallyScreen.Accounts.name).assertIsSelected()
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
+        composeTestRule.onRoot().printToLog("currentLabelExists")
+        composeTestRule.onNodeWithContentDescription(RallyScreen.Accounts.name).assertExists()
     }
 }
